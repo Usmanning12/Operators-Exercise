f538cd5a245cb481369b4abba5fbc9435e72a095
Darron Manning <darronmanning@yahoo.com>
2024-11-07 11:10:25 -0600

    OperationExercise

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace OperatorExercise
+{
+    public class Program
+    {
+
+
+        public static double AreaofCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            int a = 16;
+            int b = 20;
+            int c = 15;
+            int d = 100;
+
+            int sum = a + b;
+            int subtraction = b + a;
+            int mult = c * d;
+            int quotient = d / c;
+            int remainder = d % c;
+
+            int e = ++a * d++;
+            Console.WriteLine(e);
+            
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            
+            PrintAreaofCircle();
+        }
+        public static void PrintAreaofCircle()
+        {
+            Console.WriteLine("What is the area of your circle?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(
+                $"This is area of a circle with a radius of {radius} is {Math.Round(AreaofCircle(radius), 2)} ");
+        }
+    }
+}
